4e96e9c7c1680484519b09b67ac6310fbb9b15a0
Neegbeah Reeves <Nehemiahsal@aol.com>
2016-03-07 16:56:46 -0500

    Initial Commit

---
+++ Advance CarolinaTests/Advance_CarolinaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Advance_CarolinaTests.swift
+//  Advance CarolinaTests
+//
+//  Created by Neegbeah Reeves on 3/7/16.
+//  Copyright © 2016 Brown Box Works. All rights reserved.
+//
+
+import XCTest
+@testable import Advance_Carolina
+
+class Advance_CarolinaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
